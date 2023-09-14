fe2599f7ea9bc9b02acd4abdd539d24d48749006
Kento210 <T121015@ed.sus.ac.jp>
2023-09-15 02:46:13 +0900

    api受け取る側を追加

--- tutorial-app/app/test/page.tsx
+++ tutorial-app/app/test/page.tsx
@@ -1,4 +1,20 @@
-export default function Page() {
-  return <h1>Test Page</h1>;
+type Todo = {
+  title: string;
 }
 
+async function getData(){
+  const res = await fetch('http://localhost:3000/api/todos');
+  return await res.json();
+}
+
+export default async function Page() {
+  const todos: Todo[] = await getData();
+
+  return (
+    <div>
+      <h1>Test Page</h1>;
+      {todos.map((todo) => (
+        <div>{todo.title}</div>))}
+    </div>
+  )
+}
